3da84792a726c23b55ad370a1475cb9e4c2c4362
Webzo334 <artursjanis@yahoo.com>
2018-07-28 19:07:23 +0300

    rework validatePiece

--- dist/scripts/validatePiece.js
+++ dist/scripts/validatePiece.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const __1 = require("../");
+const __1 = require("../../");
 const isPieceValid = (grid, piece, location) => {
     const [centreY, centreX] = location;
     const { structure } = piece;
